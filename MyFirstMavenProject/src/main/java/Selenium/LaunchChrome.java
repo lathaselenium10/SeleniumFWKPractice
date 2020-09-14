d515cb746db6113a74c71fd4dafe04294ad4e6f6
Latha <lathaselenium10@gmail.com>
2020-09-14 12:49:16 -0400

    Adding the code to Github repository

---
+++ MyFirstMavenProject/src/main/java/Selenium/LaunchChrome.java
@@ -0,0 +1,19 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http:www.google.com");
+		System.out.println(driver.getTitle());
+	}
+
+}
